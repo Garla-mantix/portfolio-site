5c670cffb3a8bfcd1db9285f7820836f90d9b22c
Luca Pirro <aspudds-luca@hotmail.com>
2026-04-08 13:34:04 +0200

    Overtime project added

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -3,6 +3,15 @@ import type { Project } from '../types';
 export const projects: Project[] = [
   {
     id: '1',
+    title: 'Overtime',
+    description: 'A sports discussion forum built with Clean Architecture, as a group project for a course in Advanced Object Oriented Programming. Features user authentication with JWT, persistence via SQLite, filtering and pagination, admin panel and more. Deployed on Azure.',
+    techStack: ['C#', 'ASP.NET Core Identity', 'Entity Framework Core', 'SQLite', 'Azure'],
+    githubUrl: 'https://github.com/Garla-mantix/Overtime-Forum',
+    liveUrl: 'https://overtime-forum-ghhyh8abe3ecdzev.swedencentral-01.azurewebsites.net/',
+    imageUrl: '/overtime.png',
+  },
+  {
+    id: '2',
     title: 'Bankster',
     description: 'A personal finance management web application built with Blazor and C#. Allows users to create accounts, make transactions, and view their financial data. Deployed on Azure.',
     techStack: ['C#', 'Blazor', 'Azure', 'HTML', 'CSS',],
@@ -11,7 +20,7 @@ export const projects: Project[] = [
     imageUrl: '/bankster.png',
   },
   {
-    id: '2',
+    id: '3',
     title: 'Glamhouse Rec Website',
     description: 'Started as a project for CS50, for my friends DJ duo called Glamhouse. A simple website to showcase their music and events. Polished the frontend and responsiveness with Claude Code. Still a work in progress, that we will publish to a better domain when it is more complete.',
     techStack: ['Javascript', 'HTML', 'CSS', 'Bootstrap', 'Github Pages', 'Claude Code'],
@@ -20,9 +29,9 @@ export const projects: Project[] = [
     imageUrl: '/glamhouse.png',
   },
   {
-    id: '3',
+    id: '4',
     title: 'Dog Memory Game',
-    description: 'A memory card matching game with dog images, built as a group project for my Frontend Development course in the Spring of 2025. Features three timed difficulty levels.',
+    description: 'A memory card matching game with dog images, built as a group project for a Frontend Development course in the Spring of 2025. Features three timed difficulty levels.',
     techStack: ['React', 'Vite', 'Tailwind', 'External API', 'Git/GitHub'],
     githubUrl: 'https://github.com/Garla-mantix/dog-memory',
     liveUrl: 'https://dog-memory.vercel.app/',
